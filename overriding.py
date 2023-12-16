d0dba84a2ce211c323d3f0e5a1c4399bdd15fa7e
N Rajesh <140257593+nallagatlarajesh@users.noreply.github.com>
2023-12-17 00:20:10 +0530

    polymorphism over riding

---
+++ overriding.py
@@ -0,0 +1,17 @@
+class parent:
+    def bike(self):
+        print("i have passion pro bike")
+
+class child(parent):
+    pass
+    #def bike(self):
+        #print("i have puslar 220 bike")
+#create object
+obj1=child()
+obj1.bike()
+
+#in this we look overriding
+#and we look inherit from parent class
+#if he is not over riding then we get
+#i have pulsar 220 bike
+
